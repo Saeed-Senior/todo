5be3271ba8b8e167fc03ac181c4303ee7b791520
Saeed-Senior <nohchosaid@mail.ru>
2023-03-02 16:57:41 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <main className="flex flex-col items-center">
       {console.log(todos)}
-    <h1 className="text-center text-sky-300 font-bold text-[32px] mt-10 pb-3 border-b-2 border-gray-200 sm:w-[415px] inline-block">Количество задач:{todos.length}</h1>
+    <h1 className="text-center text-sky-300 font-bold text-[32px] sm:mt-[70px] mt-10 pb-3 border-b-2 border-gray-200 sm:w-[415px] inline-block">Количество задач:{todos.length}</h1>
     {todos.map((todo) => {
       return (
         <ToDo key={todo.id} todo = {todo} removeTask = {removeTask} handleToggle = {handleToggle}/>
